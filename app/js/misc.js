2909d9b7a655013b93cc762c0a3e891c855becb4
vankasteelj <vankasteelj@gmail.com>
2015-12-02 02:38:40 +0100

    start code for update

--- app/js/misc.js
+++ app/js/misc.js
@@ -11,7 +11,7 @@ var misc = {
         video: ['.3g2', '.3gp', '.3gp2', '.3gpp', '.60d', '.ajp', '.asf', '.asx', '.avchd', '.avi', '.bik', '.bix', '.box', '.cam', '.dat', '.divx', '.dmf', '.dv', '.dvr-ms', '.evo', '.flc', '.fli', '.flic', '.flv', '.flx', '.gvi', '.gvp', '.h264', '.m1v', '.m2p', '.m2ts', '.m2v', '.m4e', '.m4v', '.mjp', '.mjpeg', '.mjpg', '.mkv', '.moov', '.mov', '.movhd', '.movie', '.movx', '.mp4', '.mpe', '.mpeg', '.mpg', '.mpv', '.mpv2', '.mxf', '.nsv', '.nut', '.ogg', '.ogm', '.omf', '.ps', '.qt', '.ram', '.rm', '.rmvb', '.swf', '.ts', '.vfw', '.vid', '.video', '.viv', '.vivo', '.vob', '.vro', '.wm', '.wmv', '.wmx', '.wrap', '.wvx', '.wx', '.x264', '.xvid'],
         subtitle: ['.srt', '.sub', '.smi', '.txt', '.ssa', '.ass', '.mpl']
     },
-    fileType: function(file) {
+    fileType: function (file) {
         var ext = path.extname(file).toLowerCase();
         if (misc.supportedTypes.video.indexOf(ext) > -1) {
             return 'video';
@@ -21,12 +21,12 @@ var misc = {
             return null;
         }
     },
-    animate: function(el, cl, duration) {
-        $(el).addClass(cl).delay(duration).queue(function() {
+    animate: function (el, cl, duration) {
+        $(el).addClass(cl).delay(duration).queue(function () {
             $(el).removeClass(cl).dequeue();
         });
     },
-    extractQuality: function(title) {
+    extractQuality: function (title) {
         console.info('Detecting quality...');
         if (title.match(/720[pix]/i) && !title.match(/dvdrip|dvd\Wrip/i)) {
             return '720p';
@@ -36,7 +36,7 @@ var misc = {
         }
         return false;
     },
-    clearName: function(name) {
+    clearName: function (name) {
         var title = path.parse(name).name;
         return title
             .replace(/(400|480|720|1080)[pix]/gi, '') // quality clean
@@ -55,7 +55,7 @@ var misc = {
             .replace(/^\-/, '') // starts with '-'
             .replace(/ +/g, ' '); // has multiple spaces
     },
-    detect_lang: function() {
+    detect_lang: function () {
         var sub = $('#subtitle-file-path').val();
         if (!sub || sub == '') {
             misc.animate($('#subtitle-file-path'), 'warning', 1750);
@@ -64,7 +64,7 @@ var misc = {
         console.info('Detecting subtitle language...');
         $('.tooltipped').tooltip('close');
         $('.detect-lang i').addClass('fa-circle-o-notch fa-spin').removeClass('fa-magic');
-        require('detect-lang')(sub).then(function(data) {
+        require('detect-lang')(sub).then(function (data) {
             if (data && data.probability > 45 && (data.iso6392 || data.bibliographic)) {
                 $('#sublanguageid').val((data.iso6392 || data.bibliographic))
             } else {
@@ -72,19 +72,19 @@ var misc = {
                 throw 'not conclusive enough';
             }
             $('.detect-lang i').addClass('fa-magic').removeClass('fa-circle-o-notch fa-spin')
-        }).catch(function(err) {
+        }).catch(function (err) {
             $('.detect-lang i').addClass('fa-magic').removeClass('fa-circle-o-notch fa-spin');
             console.error('misc.detect_lang() error:', err);
         });
     },
-    openExternal: function(link) {
+    openExternal: function (link) {
         gui.Shell.openExternal(link);
     },
-    openImdb: function() {
+    openImdb: function () {
         var id = $('#imdb-info').attr('imdbid');
         if (id) misc.openExternal('http://www.imdb.com/title/' + id);
     },
-    restartApp: function() {
+    restartApp: function () {
         var argv = gui.App.fullArgv,
             CWD = process.cwd();
 
@@ -96,8 +96,8 @@ var misc = {
         }).unref();
         gui.App.quit();
     },
-    keyboardShortcuts: function() {
-        document.addEventListener("keypress", function(key) {
+    keyboardShortcuts: function () {
+        document.addEventListener("keypress", function (key) {
             if (key.charCode === 13) {
                 interface.keyEnter(key.target.id);
             } else if (key.ctrlKey && key.charCode === 4) {
@@ -106,14 +106,14 @@ var misc = {
                 misc.restartApp();
             }
         });
-        document.addEventListener("keyup", function(key) {
+        document.addEventListener("keyup", function (key) {
             if (key.keyCode === 27) {
                 $('#search-popup').css('opacity', 0).hide();
                 interface.reset('search');
             }
         });
     },
-    getSelection: function(textbox) {
+    getSelection: function (textbox) {
         var selectedText = null;
         var activeElement = document.activeElement;
 
@@ -128,6 +128,29 @@ var misc = {
         }
 
         return selectedText;
+    },
+    checkUpdates: function () {
+        //if (parseInt(localStorage.lastUpdateCheck) + 604800000 > Date.now()) return;
+
+        localStorage.lastUpdateCheck = Date.now();
+
+        var url = 'https://raw.githubusercontent.com/vankasteelj/opensubtitles-uploader/master/package.json';
+        require('https').get(url, function (res) {
+            var body = '';
+            res.on('data', function (chunk) {
+                body += chunk.toString();
+            });
+            res.on('end', function () {
+                var avail_version = JSON.parse(body).version;
+                if (avail_version > version) {
+                    console.info('update available:', avail_version);
+                } else {
+                    console.log('no update available');
+                }
+            });
+        }).on('error', function (e) {
+            console.error(e);
+        });
     }
 };
 
@@ -135,7 +158,7 @@ var misc = {
 /*
  * remember positionning
  */
-win.on('move', function(x, y) {
+win.on('move', function (x, y) {
     localStorage.posX = Math.round(x);
     localStorage.posY = Math.round(y);
 });
@@ -144,40 +167,40 @@ win.on('move', function(x, y) {
 /*
  * drag & drop
  */
-window.addEventListener('dragover', function(e) {
+window.addEventListener('dragover', function (e) {
     e.preventDefault();
     e.stopPropagation();
 }, false);
-window.addEventListener('dragstart', function(e) {
+window.addEventListener('dragstart', function (e) {
     e.preventDefault();
     e.stopPropagation();
 }, false);
-window.addEventListener('drop', function(e) {
+window.addEventListener('drop', function (e) {
     e.preventDefault();
     e.stopPropagation();
 }, false);
 
-window.ondragenter = function(e) {
+window.ondragenter = function (e) {
     $('#drop-mask').show();
     var showDrag = true;
     var timeout = -1;
     $('#drop-mask').on('dragenter',
-        function(e) {
+        function (e) {
             misc.type = misc.fileType(e.originalEvent.dataTransfer.files[0].name);
             if (misc.type) {
                 $('#main-' + misc.type).css('border-color', '#4A6B8A');
             }
         }.bind(this));
     $('#drop-mask').on('dragover',
-        function(e) {
+        function (e) {
             var showDrag = true;
         });
 
     $('#drop-mask').on('dragleave',
-        function(e) {
+        function (e) {
             var showDrag = false;
             clearTimeout(timeout);
-            timeout = setTimeout(function() {
+            timeout = setTimeout(function () {
                 if (!showDrag) {
                     $('#main-' + misc.type).css('border-color', '');
                     misc.type = null;
@@ -185,7 +208,7 @@ window.ondragenter = function(e) {
             }, 10);
         });
 };
-window.ondrop = function(e) {
+window.ondrop = function (e) {
     e.preventDefault();
     $('#drop-mask').hide();
 
