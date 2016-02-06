aadbfb66ec7aab1dfc578802604d1da09b338091
vankasteelj <vankasteelj@gmail.com>
2016-02-06 19:52:35 +0100

    moving variables

--- app/js/misc.js
+++ app/js/misc.js
@@ -1,13 +1,3 @@
-/*
- * important variables used in the app
- */
-var gui = require('nw.gui'),
-    win = gui.Window.get(),
-    data_path = gui.App.dataPath,
-    path = require('path'),
-    fs = require('fs'),
-    Promise = require('bluebird');
-
 var misc = {
     supportedTypes: {
         video: ['.3g2', '.3gp', '.3gp2', '.3gpp', '.60d', '.ajp', '.asf', '.asx', '.avchd', '.avi', '.bik', '.bix', '.box', '.cam', '.dat', '.divx', '.dmf', '.dv', '.dvr-ms', '.evo', '.flc', '.fli', '.flic', '.flv', '.flx', '.gvi', '.gvp', '.h264', '.m1v', '.m2p', '.m2ts', '.m2v', '.m4e', '.m4v', '.mjp', '.mjpeg', '.mjpg', '.mkv', '.moov', '.mov', '.movhd', '.movie', '.movx', '.mp4', '.mpe', '.mpeg', '.mpg', '.mpv', '.mpv2', '.mxf', '.nsv', '.nut', '.ogg', '.ogm', '.omf', '.ps', '.qt', '.ram', '.rm', '.rmvb', '.swf', '.ts', '.vfw', '.vid', '.video', '.viv', '.vivo', '.vob', '.vro', '.wm', '.wmv', '.wmx', '.wrap', '.wvx', '.wx', '.x264', '.xvid'],
